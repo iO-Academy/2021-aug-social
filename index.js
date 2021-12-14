f6ef9dc2481f4adb984328ecc30004b491c0c964
kimberley <kim.dobney@yahoo.co.uk>
2021-12-14 11:48:37 +0000

    connected to sql database

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const mysql = require('promise-mysql')
+
 
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => {
-    res.send('Hello super lynx')
+app.get('/', async (req, res) => {
+    // res.send('Hello super lynx')
+    const connection = await mysql.createConnection({
+        user: 'root',
+        password: 'password',
+        database: 'social-app'
+    })
+
+    const users = await connection.query('SELECT * FROM `users`;')
+
+    res.json(users)
+    console.log(users)
+    // res.send(users)
 })
+
+
 // The app.listen() function is used to bind and listen the
 // connections on the specified host and port.
 app.listen(port)
