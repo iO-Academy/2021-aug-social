f5bdbc87ef310ddcb66add8fb5a030de52926e96
charliecog <charlie.coggans@io-academy.uk>
2021-12-15 15:47:28 +0000

    adds auth routes using JWT

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const mysql = require('promise-mysql')
+const expressjwt = require("express-jwt");
+const jwt = require("jsonwebtoken");
 
 const app = express()
 const port = 8000
 app.use(express.json())
+
+const errorCheck = function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.json({success: false, status: 401, message: 'invalid token'})
+    }
+};
+
+const jwtCheck = expressjwt({
+    secret: "mysupersecretkey",
+    algorithms: ['HS256']
+});
+
 
 app.get('/profile/:id', async (req,res) => {
     const connection = await mysql.createConnection({
@@ -38,16 +52,67 @@ app.post('/signUp', async (req,res) => {
     })
 
     const result = await connection.query("INSERT INTO users (username, bio, email, password) VALUES ('" + username + "', '" + bio + "', '" + email + "', '" + password + "' )")
-    if (result){
-        res.json({success: true, data:result})
-        console.log('item added')
-    } else {
-        res.json({success: false, data:result})
-        console.log('item not added')
+
+    if (!result){
+        res.json({success: false, message: "something went wrong"})
+        return
     }
+
+    const token = jwt.sign({
+        sub: result.insertId,
+        username: username,
+        email: email
+    }, "mysupersecretkey", {expiresIn: "3 hours"});
+
+    res.json({success: true, message: 'successfully logged in', data: {username, email, bio, access_token: token}})
+
 })
 
+app.post('/login', async (req,res) => {
+    //check to see that the credentials are being sent in the body of the req
+    //create the connection to the db
+    //grab all the users from the db
+    //search through the users for someone that matches the provided credentials
+    //if no one matches send a fail response
+    //if someone matches then create a new token and send a success response
 
+    if (!req.body.email || !req.body.password) {
+        res.json({success: false, message: 'please send all crendentials needed'})
+        return;
+    }
+
+    let {email, password} = req.body
+
+    const connection = await mysql.createConnection({
+        user: 'root',
+        password: 'password',
+        database: 'social-app'
+    })
+
+    const users = await connection.query('select `username`, `bio`, `email`, `password` from `users`')
+
+    const user = users.find((u) => {
+        return u.email === req.body.email && u.password === req.body.password;
+    });
+
+    if (!user) {
+        res.json({success: false, message: 'couldnt find anyone that matches your credentials'})
+        return;
+    }
+
+    const token = jwt.sign({
+        sub: user.id,
+        username: user.username,
+        email: user.email
+    }, "mysupersecretkey", {expiresIn: "3 hours"});
+
+    res.json({success: true, message: 'successfully logged in', data: {user, access_token: token}})
+
+})
+
+app.get('/secret', jwtCheck, errorCheck, (req, res) => {
+    res.json({message: 'hello'})
+})
 
 // The app.listen() function is used to bind and listen the
 // connections on the specified host and port.
