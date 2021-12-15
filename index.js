0fe71e8f1224d4d9e2bb8ded362348b636534b08
DevUsman-18 <mohammad_usman117@hotmail.co.uk>
2021-12-15 12:10:10 +0000

    sql statements working

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 const express = require('express')
 const mysql = require('promise-mysql')
 
-
 const app = express()
 const port = 8000
 app.use(express.json())
-
-// app.get('/profile/:id', async (req, res) => {
-//     // res.send('Hello super lynx')
-//     const connection = await mysql.createConnection({
-//         user: 'root',
-//         password: 'password',
-//         database: 'social-app'
-//     })
-//     //get infor using specific id
-//     //get username, bio from users
-// // and display content, user-id, date-time from posts
-//     const id = req.params.id
-//     const users = await connection.query('SELECT `username`, `bio`, `user-following`.`following-id`, `posts`.`content` FROM `users`\n' +
-//         '\tLEFT JOIN `user-following`\n' +
-//         '\t\tON `users`.`id` = `user-following`.`user-id`\n' +
-//         '\tLEFT JOIN `posts`\n' +
-//         '\t\tON `users`.`id` = `posts`.`user-id`\n' +
-//         '\tWHERE `users`.`id` = 1')
-//     res.json(users)
-//     console.log(users)
-// })
 
 app.get('/profile/:id', async (req,res) => {
     const connection = await mysql.createConnection({
@@ -50,7 +28,6 @@ app.get('/following/:id', async (req,res) => {
     res.json({following: following})
 })
 
-
 app.post('/signUp', async (req,res) => {
     let {username, bio, email, password} = req.body
 
@@ -60,29 +37,16 @@ app.post('/signUp', async (req,res) => {
         database: 'social-app'
     })
 
-    console.log(username)
-    console.log(bio)
-    console.log(email)
-    console.log(password)
-
-
-    // const newUser = {
-    //     username: req.body.username,
-    //     bio: req.body.bio,
-    //     email: req.body.email,
-    //     password: req.body.password
-    // }
-    const result = await connection.query("INSERT INTO users (username, bio, email, password) VALUES ('"+ ${username} +"','"+ ${bio} +"', '" + ${email} + "','" + ${password} + "');")
-    // if (result){
-    //     res.json({success: true, data:result})
-    //     console.log('itemadded')
-    // } else {
-    //     res.json({success: false, data:result})
-    //     console.log('itemnotadded')
-    // }
-
-
+    const result = await connection.query("INSERT INTO users (username, bio, email, password) VALUES ('" + username + "', '" + bio + "', '" + email + "', '" + password + "' )")
+    if (result){
+        res.json({success: true, data:result})
+        console.log('item added')
+    } else {
+        res.json({success: false, data:result})
+        console.log('item not added')
+    }
 })
+
 
 
 // The app.listen() function is used to bind and listen the
